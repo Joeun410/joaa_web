72a782d78764b2c04e291cbc537eb64f4e921843
Joeun410 <76474050+Joeun410@users.noreply.github.com>
2022-02-08 15:40:48 +0900

    웹테스트

---
+++ pageMove.js
@@ -0,0 +1,17 @@
+function moveSubPage(name){
+    window.parent.location.href= "sub_html/" + name + ".html";
+}
+
+function movePageGraphic(name){
+    window.parent.location.href=  "../"+ "sub_html/"+ "graphic/"+ name + ".html";
+}
+function movePage3d(name){
+    window.parent.location.href=  "../"+ "sub_html/"+name + ".html";
+}
+function movePageCoding(name){
+    window.parent.location.href=  "../"+ "sub_html/"+name + ".html";
+}
+
+function moveHome(name){
+    window.parent.location.href=  "../"+ "../" + name + ".html";
+}
